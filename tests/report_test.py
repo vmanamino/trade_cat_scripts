4d1225bdef0287e3a57783917634cd6d278f932a
vao1843 <vmanamino@gmail.com>
2017-08-25 17:12:05 +0200

    in prog

--- tests/report_test.py
+++ tests/report_test.py
@@ -13,17 +13,21 @@ from product import Product
 data = get_sheetdata('C:\\Code\\trade_cat_scripts\\tests\\dataset\\dataset2017.xlsx')
 item = BFLUXItem(data, 12107)
 
-product = Product(item.isbn)
+with open('dataset\\vlb_test_pickle.txt', 'rb') as content:
+	product_data = pickle.load(content)
+
+product = Product(product_data)
 
 report = Report('isbn check', 'VLB')
 
-outsheet = report.generate(2, item, book)
+outsheet = report.generate(2, item, product)
 
 class reportTests(unittest.TestCase):
 	
-	self.assertTrue(outsheet.cell(row=2, column=1) == item.isbn)
-	self.assertTrue(outsheet.cell(row=2, column=2) == item.title)
-	self.assertTrue(outsheet.cell(row=2, column=3) == item.imprint)
-	self.assertTrue(outsheet.cell(row=2, column=4) == book.isbn)
-	self.assertTrue(outsheet.cell(row=2, column=5) == book.title)
-	self.assertTrue(outsheet.cell(row=2, column=1) == item.isbn)
+	def testProperties(self):
+		self.assertTrue(outsheet.cell(row=2, column=1) == item.isbn)
+		self.assertTrue(outsheet.cell(row=2, column=2) == item.title)
+		self.assertTrue(outsheet.cell(row=2, column=3) == item.imprint)
+		self.assertTrue(outsheet.cell(row=2, column=4) == product.isbn)
+		self.assertTrue(outsheet.cell(row=2, column=5) == product.title)
+		self.assertTrue(outsheet.cell(row=2, column=1) == item.isbn)
